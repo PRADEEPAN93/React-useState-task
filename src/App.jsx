ca02c295c7162b675b7b62f46c42681a5880279b
PRADEEPAN93 <pradeepank6070@gmail.com>
2025-11-19 12:38:15 +0530

    add one app.css file using routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-
-// import './App.css'
-// import './Form.css'
-import './Advice.css';
+import './App.css';
 import { QrCode } from './Component/QrCode';
 import { Counter } from './FormSample/Counter';
 import { NormalUserDetails } from './FormSample/NormalUserDetails';
 import RegForm from './FormSample/RegForm';
 import { AdviceApp } from './AdviceAppApi/AdviceApp';
 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
- 
 
   return (
-    <>
-      <QrCode />
-      <Counter />
-      <NormalUserDetails />
-      <RegForm />
-      <AdviceApp />
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <Link to="/">QrCode</Link> |{" "}
+        <Link to="/Counter">Counter</Link> |{" "}
+        <Link to="/NormalUser">Normal User</Link> |{" "}
+        <Link to="/RegForm">Registration Form</Link> |{" "}
+        <Link to="/AdviceApp">Advice App</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<QrCode />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="/NormalUser" element={<NormalUserDetails />} />
+        <Route path="/RegForm" element={<RegForm />} />
+        <Route path="/AdviceApp" element={<AdviceApp />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
